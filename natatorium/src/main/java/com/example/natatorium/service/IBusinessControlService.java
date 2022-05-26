70ba6947c27e710e45f4593c65cb29340a2dc1a6
gyhwalker <gyh_walker@163.com>
2022-05-26 19:22:41 +0800

    初步完成

---
+++ natatorium/src/main/java/com/example/natatorium/service/IBusinessControlService.java
@@ -0,0 +1,13 @@
+package com.example.natatorium.service;
+
+import com.example.natatorium.entity.BusinessControl;
+import com.example.natatorium.common.base.BaseIService;
+
+/**
+* @author GuYuHang
+* @description 针对表【business_control(业务总控)】的数据库操作Service
+* @createDate 2022-04-20 09:27:44
+*/
+public interface IBusinessControlService extends BaseIService<BusinessControl> {
+
+}
---
+++ natatorium/src/main/java/com/example/natatorium/service/IBusinessListService.java
@@ -0,0 +1,13 @@
+ package com.example.natatorium.service;
+
+import com.example.natatorium.common.base.BaseIService;
+import com.example.natatorium.entity.BusinessList;
+
+/**
+* @author HP
+* @description 针对表【business_list(业务卡列表)】的数据库操作Service
+* @createDate 2022-04-10 17:53:31
+*/
+public interface IBusinessListService extends BaseIService<BusinessList> {
+
+}
